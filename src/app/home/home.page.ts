bdee881adce2ebd78c26bd7abcb448afef47cefe
ashirka03 <canoalejandra877@gmail.com>
2023-03-19 17:36:24 -0600

    Commit inicial del proyecto Angular-campus

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { InAppBrowser } from '@awesome-cordova-plugins/in-app-browser/ngx';
+import { App } from '@capacitor/app';
+import { SplashScreen } from '@capacitor/splash-screen';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  constructor(private browser: InAppBrowser) {}
 
-  constructor() {}
+  ngOnInit(): void {
+    const browserInstance = this.browser.create(
+      'https://campus.vivanlosjovenes.edu.gt', 
+      '_self',
+      'location=no, hidenavigationbuttons=true,hideurlbar=truw.zoom=no'
+    );
 
+    browserInstance.on('loadstop').subscribe((evt) => {
+      SplashScreen.hide();
+    });
+    
+    browserInstance.on('loaderror').subscribe((evt) => {
+      SplashScreen.hide();
+    });
+
+
+    browserInstance.on('exit').subscribe((evt) => {
+      App.exitApp();
+    });
+  }
 }
